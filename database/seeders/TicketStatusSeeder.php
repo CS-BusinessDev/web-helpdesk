a7e30f9208b9509955d53aff187e712880a01c22
apriansyahrs <apriansyahrizs@gmail.com>
2024-06-11 09:46:31 +0700

    Menyesuaikan data status yang ada pada lapangan

--- database/seeders/TicketStatusSeeder.php
+++ database/seeders/TicketStatusSeeder.php
@@ -14,12 +14,8 @@ class TicketStatusSeeder extends Seeder
     {
         TicketStatus::insert([
             ['id' => TicketStatus::OPEN, 'name' => 'Open'],
-            ['id' => TicketStatus::ASSIGNED, 'name' => 'Assigned'],
             ['id' => TicketStatus::IN_PROGRESS, 'name' => 'In Progress'],
-            ['id' => TicketStatus::ON_HOLD, 'name' => 'On Hold'],
-            ['id' => TicketStatus::ESCALATED, 'name' => 'Escalated'],
-            ['id' => TicketStatus::PENDING_CUSTOMER_RESPONSE, 'name' => 'Pending Customer Response'],
-            ['id' => TicketStatus::RESOLVED, 'name' => 'Resolved'],
+            ['id' => TicketStatus::CANCEL, 'name' => 'Cancel'],
             ['id' => TicketStatus::CLOSED, 'name' => 'Closed'],
         ]);
     }
