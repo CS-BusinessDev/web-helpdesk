6fbdd90daea94896f60514b14157520e75d94024
apriansyahrs <apriansyahrizs@gmail.com>
2024-06-13 13:13:25 +0700

    bug user dapet ngedit penanggung jawab dan status

--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -122,12 +122,12 @@ class TicketResource extends Resource
                             ->pluck('name', 'id'))
                         ->searchable()
                         ->required()
-                        ->hiddenOn('create'),
-                        // ->hidden(
-                        //     fn () => !auth()
-                        //         ->user()
-                        //         ->hasAnyRole(['Super Admin', 'Admin Unit', 'Staff Unit']),
-                        // ),
+                        ->hiddenOn('create')
+                        ->hidden(
+                            fn () => !auth()
+                                ->user()
+                                ->hasAnyRole(['Super Admin', 'Admin Unit', 'Staff Unit']),
+                        ),
 
                     Forms\Components\Select::make('responsible_id')
                         ->label(__('Responsible'))
@@ -135,12 +135,12 @@ class TicketResource extends Resource
                             ->pluck('name', 'id'))
                         ->searchable()
                         ->required()
-                        ->hiddenOn('create'),
-                        // ->hidden(
-                        //     fn () => !auth()
-                        //         ->user()
-                        //         ->hasAnyRole(['Super Admin', 'Admin Unit']),
-                        // ),
+                        ->hiddenOn('create')
+                        ->hidden(
+                            fn () => !auth()
+                                ->user()
+                                ->hasAnyRole(['Super Admin', 'Admin Unit']),
+                        ),
 
                     Forms\Components\Placeholder::make('created_at')
                         ->translateLabel()
